83c2497d9b47402ed1e4bf9c2d3b1bebfad3972f
JessicaAgustin <jessica_agustin@questronix.com.ph>
2019-05-01 11:14:35 +0800

    Sync develop branch

---
+++ services/Errors.js
@@ -0,0 +1,55 @@
+module.exports = {
+    get: function (tag) {
+      var errors = {
+        MISSING_INVALID_PARAMS: { 
+          status: 400, 
+          error: { 
+            code: -1, 
+            message: 'Missing/invalid parameters.', 
+            params: [] 
+          } 
+        },
+        INTERNAL_SERVER_ERROR: {
+          status: 500, 
+          error: {
+            code: -2, 
+            message: 'Internal server error.'
+          }
+        },
+        NOT_FOUND: {
+          status: 404, 
+          error: { 
+            code: -3, 
+            message: "Not found."
+          }
+        },
+        SERVER_ERROR: {
+          status: 500,
+          error: {
+            code: -4,
+            message: 'Server unreachable.'
+          }
+        },
+        SERVICE_ERROR: {
+          status: 500,
+          error: {
+            code: -5,
+            message: 'Service error/unavailable.'
+          }
+        },
+        UNAUTHORIZED_ACCESS: {
+          status: 401,
+          error: {
+            code: -6,
+            message: 'Unauthorized Access.'
+          }
+        }
+      };
+      return errors[tag];
+    },
+    raise: function (e) {
+      var error = JSON.parse(JSON.stringify(this.get(e)));
+      error.error.details = {source: 'test'};
+      return error;
+    }
+  };
